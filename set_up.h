c2351bf7682522ef3268ef758ceee6128759ba6b
leenuu <61344683+leenuu@users.noreply.github.com>
2021-07-18 16:23:51 +0900

    Update set_up.h

--- set_up.h
+++ set_up.h
@@ -1,11 +1,15 @@
 #pragma once
 int map[8][8];
+std::string chess_piece_whilte[13] = {"", "▽", "♤", "○", "◇", "♡", "☆", "▲", "♠", "●", "◆", "♥", "★" };
+
 
 void re_map();
+void set_piece();
 
 void step_up()
 {
 	re_map();
+	set_piece();
 }
 
 void cursorview(bool show)
@@ -43,20 +47,44 @@ void re_map()
 	}
 }
 
+void set_piece()
+{
+	for (int xl = 0; xl < 5; xl++)
+	{	
+		int x = xl + 2;
+		map[0][xl] = x;
+		map[7][xl] = x + 6;
+		map[0][7 - xl] = x;
+		map[7][7 - xl] = x + 6;
+	}
+	
+	for (int x = 0; x < 8; x++)
+	{
+		map[1][x] = 1;
+		map[6][x] = 7;
+	}
+}
 
 void print_map()
 {
-	for (int y = 0; y < 8; y++)
+	std::cout << "    1    2    3    4    5    6    7    8";
+	
+	for (int yl = 1; yl < 17; yl +=2)
 	{
-		std::cout << " ---- ---- ---- ---- ---- ---- ---- ---- " << std::endl;
+		int y = (yl - 1) / 2;
+		gotoxy(3, yl);
+		std::cout << " ---- ---- ---- ---- ---- ---- ---- ---- ";
+		gotoxy(1, yl + 1);
+		std::cout << y+1;
+		gotoxy(3, yl + 1);
 		std::cout << "| ";
+		
 		for (int x = 0; x < 8; x++)
-		{
-
-			if ( map[y][x] == 0 )  
+		{	
+			if (map[y][x] == 0)
 			{
 				int i = x + y;
-				
+
 				if (i % 2 == 0)
 				{
 					std::cout << "□";
@@ -66,12 +94,60 @@ void print_map()
 					std::cout << "■";
 				}
 			}
+			else if(map[y][x] == 1)
+			{
+				std::cout << "▽";
+			}
+			else if (map[y][x] == 2)
+			{
+				std::cout << "♤";
+			}
+			else if (map[y][x] == 3)
+			{
+				std::cout << "○";
+			}
+			else if (map[y][x] == 4)
+			{
+				std::cout << "◇";
+			}
+			else if (map[y][x] == 5)
+			{
+				std::cout << "♡";
+			}
+			else if (map[y][x] == 6)
+			{
+				std::cout << "☆";
+			}
+			else if (map[y][x] == 7)
+			{
+				std::cout << "▲";
+			}
+			else if (map[y][x] == 8)
+			{
+				std::cout << "♠";
+			}
+			else if (map[y][x] == 9)
+			{
+				std::cout << "●";
+			}
+			else if (map[y][x] == 10)
+			{
+				std::cout << "◆";
+			}
+			else if (map[y][x] == 11)
+			{
+				std::cout << "♥";
+			}
+			else if (map[y][x] == 12)
+			{
+				std::cout << "★";
+			}
+
 			std::cout << " | ";
 		}
-		
-		std::cout << std::endl;
 	}
-	std::cout << " ---- ---- ---- ---- ---- ---- ---- ---- " << std::endl;
+	gotoxy(3, 17);
+	std::cout << " ---- ---- ---- ---- ---- ---- ---- ---- ";
 }
 
 void test()
